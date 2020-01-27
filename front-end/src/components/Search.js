54b7775770c43a4ad06f1e9dd89e6fb26860eddd
Scott Malmberg <scott.malmberg@icloud.com>
2020-01-27 08:56:33 -0800

    hiding search on mobile :(

--- front-end/src/components/Search.js
+++ front-end/src/components/Search.js
@@ -3,11 +3,10 @@ import React, { Fragment } from 'react';
 
 
 const Search = (props) => {
-
-    
+  
     return (
         <Fragment>
-            <input type="text" name="search" placeholder="What sounds good?" value={props.filter} onChange={props.handleChange} className="form-control mb-4" />
+            <input type="text" id="search" name="search" placeholder="What sounds good?" value={props.filter} onChange={props.handleChange} className="form-control mb-4" />
         </Fragment> 
     )
 }
